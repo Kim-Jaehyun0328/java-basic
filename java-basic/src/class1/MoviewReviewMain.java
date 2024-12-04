e7e8cb35c2d3afb465d152d029b223c8cafef5ad
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-12-04 22:22:26 +0900

    class1

---
+++ java-basic/src/class1/MoviewReviewMain.java
@@ -0,0 +1,22 @@
+package class1;
+
+public class MoviewReviewMain {
+
+    public static void main(String[] args) {
+        MovieReview movieReview1 = new MovieReview();
+        MovieReview movieReview2 = new MovieReview();
+
+        movieReview1.title = "인셉션";
+        movieReview1.review = "인생은 무한 루프";
+
+        movieReview2.title = "어바웃 타임";
+        movieReview2.review = "인생 시간 영화!";
+
+        MovieReview[] movieReviews = {movieReview1, movieReview2};
+
+        for (MovieReview mr : movieReviews) {
+            System.out.println("영화 제목: " + mr.title + ", 리뷰: " + mr.review);
+        }
+
+    }
+}
